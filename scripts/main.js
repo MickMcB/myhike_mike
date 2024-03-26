d521b5a2747a6cc8c681acf287215f45e0561d09
Michael McBride <mcbride1987@gmail.com>
2024-03-26 12:52:27 -0700

    update for demo11

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
+//Global variable pointing to the current user's Firestore document
+var currentUser;
+
+//Function that calls everything needed for the main page  
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+
+            // figure out what day of the week it is today
+            const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+            const d = new Date();
+            let day = weekday[d.getDay()];
+
+            // the following functions are always called when someone is logged in
+            readQuote(day);
+            insertNameFromFirestore();
+            displayCardsDynamically("hikes");
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "login.html";
+        }
+    });
+}
+doAll();
+
 // function getNameFromAuth() {
 //     firebase.auth().onAuthStateChanged(user => {
 //         if (user) {
@@ -29,7 +57,6 @@ function insertNameFromFirestore() {
     })
 }
 
-insertNameFromFirestore();
 // getNameFromAuth()
 
 // Function to read the quote of the day from the Firestore "quotes" collection
@@ -46,7 +73,6 @@ function readQuote(day) {
             //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
         })
 }
-readQuote("tuesday");        //calling the function
 
 function writeHikes() {
     //define a variable for the collection you want to create in Firestore to populate data
@@ -99,7 +125,10 @@ function writeHikes() {
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
 
-    db.collection(collection).get()   //the collection called "hikes"
+    db.collection(collection)
+        .orderBy("hike_time")
+        .limit(2)
+        .get()   //the collection called "hikes"
         .then(allHikes => {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allHikes.forEach(doc => { //iterate thru each doc
@@ -112,10 +141,22 @@ function displayCardsDynamically(collection) {
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
+                newcard.querySelector('.card-length').innerHTML =
+                    "Length: " + doc.data().length + " km <br>" +
+                    "Duration: " + doc.data().hike_time + "min <br>" +
+                    "Last updated: " + doc.data().last_updated.toDate().toLocaleDateString();
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
                 newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+                newcard.querySelector('i').onclick = () => updateBookmark(docID);//for backend purposes
+                newcard.querySelector('i').id = "save-" + docID;
+
+                currentUser.get().then(userDoc => {
+                    let bookmarks_now = userDoc.data().bookmarks;
+                    if (bookmarks_now.includes(docID)) {
+                        document.getElementById("save-" + docID).innerText = 'bookmark';
+                    }
+                })
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
@@ -130,5 +171,26 @@ function displayCardsDynamically(collection) {
         })
 }
 
-displayCardsDynamically("hikes");  //input param is the name of the collection
+function updateBookmark(hikeID) {
+    currentUser.get().then(userDoc => {
+        let bookmarksNow = userDoc.data().bookmarks;
+        console.log(bookmarksNow);
+        if (bookmarksNow.includes(hikeID)) {
+            console.log("this hike should exists, and should be removed")
+        } else {
+            console.log("this hike should be added")
+        }
+    })
+
+
+
+    // currentUser.update({
+    //     bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeID)
+    // })
+    //     .then(function () {
+    //         console.log("bookmark has been saved for" + hikeID)
+    //         let iconID = 'save-' + hikeID;
+    //         document.getElementById(iconID).innerText = 'bookmark';
+    //     })
+}
 
